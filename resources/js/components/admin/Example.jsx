29703795cb1834c05ffc284a741549ae8e8eef43
Hoangnhh <thit3chii@gmail.com>
2024-11-10 15:22:30 +0700

    example

---
+++ resources/js/components/admin/Example.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminLayout from './Layout/AdminLayout';
+
+const Dashboard = () => {
+    return (
+        <AdminLayout>
+            <h1>DASHBOARD</h1>
+        </AdminLayout>
+    );
+};
+
+export default Dashboard; 
