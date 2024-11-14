014b37df64eee32290637ded4c6fc864a02683e3
PC-1111\Admin <hoangnguyen>
2024-11-14 21:11:42 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,14 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://laido.invade.vn'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
+    'access_control_allow_credentials' => true,
+    'add_http_headers' => true
 ]; 
