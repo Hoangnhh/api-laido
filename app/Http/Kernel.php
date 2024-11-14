216d5d07d6dc92d1abf52fb021acfc0033ea7b32
PC-1111\Admin <hoangnguyen>
2024-11-14 21:08:40 +0700

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,6 +30,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\Cors::class,
     ];
 
     protected $middlewareGroups = [
