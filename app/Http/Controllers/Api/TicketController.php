0c8718a0c16219d045a31882863460ec5b1de89e
Hoangnhh <thit3chii@gmail.com>
2024-12-17 08:58:52 +0700

    1

--- app/Http/Controllers/Api/TicketController.php
+++ app/Http/Controllers/Api/TicketController.php
@@ -77,7 +77,7 @@ class TicketController extends Controller
 
                 // kiểm tra thời gian checkin vé
                 $checkinTicketRangeTime = $systemConfigs[SystemConfigKey::CHECKIN_TICKET_RANGE_MINUTE->value];
-                if($checkinTicketRangeTime > 0 && !str_starts_with($ticketData['service_name'], 'LV')) {
+                if($checkinTicketRangeTime > 0 && !str_starts_with($request->code, 'LV')) {
                     $timeDiff = abs(Carbon::now()->diffInMinutes($activeAssignment->checkin_at));
                     if ($timeDiff > $checkinTicketRangeTime) {
                         return $this->errorResponse("Quá thời gian cho phép quét vé, Chỉ được phép quét vé trong ({$checkinTicketRangeTime} phút)");
