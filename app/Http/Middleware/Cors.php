920f5e6ee1d1e0a5cdb0ca5c02f509abf5f93ab8
PC-1111\Admin <hoangnguyen>
2024-12-30 10:20:51 +0700

    1

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,7 +10,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'https://laido.invade.vn')
+            ->header('Access-Control-Allow-Origin', ['https://laido.invade.vn', 'http://localhost:3000'])
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
             ->header('Access-Control-Allow-Credentials', 'true');
