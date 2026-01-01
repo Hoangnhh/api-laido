a6f10f91d12322dbae1550041750fce51c2c796d
Thuy PD <thuypd.real@gmail.com>
2026-01-01 11:29:17 +0700

    ok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') { // <-- Chỉ áp dụng cho production
-            URL::forceScheme('https'); // <-- Thêm dòng này
+        if (config('app.env') === 'production' || str_contains(config('app.url'), 'https')) {
+            URL::forceScheme('https');
         }
     }
 }
